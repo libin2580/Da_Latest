c4d2e32c3154fe3f72c970217ae70e8b6104933d
Binu Lal <ansal@cybraum.net>
2018-04-17 14:24:55 +0530

    String ansal

--- app/src/main/java/com/meridian/dateout/MainActivity.java
+++ app/src/main/java/com/meridian/dateout/MainActivity.java
@@ -4,9 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-   String hi;
-   String libz;
-String rincy;
+
+   String ansal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
