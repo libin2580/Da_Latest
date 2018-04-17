3cd15b9e18bad02a8c9acabf233420bfc0f5b520
Binu Lal <ansal@cybraum.net>
2018-04-17 14:04:27 +0530

    delete code in constant

--- app/src/main/java/com/meridian/dateout/Constants.java
+++ app/src/main/java/com/meridian/dateout/Constants.java
@@ -5,8 +5,7 @@ package com.meridian.dateout;
  */
 
 public class Constants {
-    public  static  String URL="http://www.dateout.co.php56-27.phx1-2.websitetestlink.com/services/";//http://wildlyyours.com.php56-11.dfw3-2.websitetestlink.com/services/
-
-    public  static  String URL1="http://www.dateout.co.php56-27.phx1-2.websitetestlink.com/services/";//http://wildlyyours.com.php56-11.dfw3-2.websitetestlink.com/services/
+    public  static  String URL="http://www.dateout.co.php56-27.phx1-2.websitetestlink.com/services/";
+    public  static  String URL1="http://www.dateout.co.php56-27.phx1-2.websitetestlink.com/services/";
 
 }
