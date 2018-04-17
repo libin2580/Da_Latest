800a2fd131d315e96ac5be7286d8894579c83df0
Rincy <rincy@cybraum.net>
2018-04-17 14:17:43 +0530

    one string has been added

--- app/src/main/java/com/meridian/dateout/MainActivity.java
+++ app/src/main/java/com/meridian/dateout/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
    String hi;
+   String libz;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
