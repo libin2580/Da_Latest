9706ce97026870271eb8946400aa60afa237d497
DESKTOP-V6J931A\libin <anvin@cybraum.net>
2018-04-17 17:26:48 +0530

    one string has been changed

---
+++ app/src/main/java/com/meridian/dateout/Test.java
@@ -0,0 +1,18 @@
+package com.meridian.dateout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by libin on 4/17/2018.
+ */
+
+public class Test extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
